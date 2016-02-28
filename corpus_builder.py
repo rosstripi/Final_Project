d7a310c9c337e8c893333e1c9e029afb15c7d650
Ross Tripi <rosstripi@gmail.com>
2016-02-27 20:30:50 -0500

    removed concurrency in tweet streaming because it caused too many 420 errors.
    modified processing script to count bigrams and co-occurrences as well as singletons.

--- corpus_builder.py
+++ corpus_builder.py
@@ -25,15 +25,11 @@ api = tweepy.API(auth)
 
 class TweetListener(StreamListener):
     """Class to pull tweets as they appear"""
-    filename = 'stream'
-
-    def __init__(self, filename):
-        self.filename = filename
 
     def on_data(self, data):
         try:
-            os.makedirs(os.path.dirname('corpora/{}.json'.format(self.filename)), exist_ok=True)
-            with open('corpora/{}.json'.format(self.filename), 'a') as f:
+            os.makedirs(os.path.dirname('corpora/all_candidates.json'), exist_ok=True)
+            with open('corpora/all_candidates.json', 'a') as f:
                 f.write(data)
                 return True
         except BaseException as ex:
@@ -43,51 +39,16 @@ class TweetListener(StreamListener):
     def on_error(self, status):
         print(status)
         if status == 420:
-            print("\tNumber of allowed connections for time window exceeded for " + self.filename)
+            print("\tNumber of allowed connections for time window exceeded")
             return False  # returning False in on_data disconnects the stream
         return True
 
 
-def carson_fetch():
-    carson_stream = Stream(auth, TweetListener("carson")).filter(track=['#Carson'], async=True)
+candidates_stream = Stream(auth, TweetListener()).filter(track=['#Carson', 'Carson', '#BenCarson', 'Ben Carson',
+                                                                        '#Clinton', 'Clinton', '#Hillary', 'Hillary', '#HillaryClinton', 'Hillary Clinton',
+                                                                        '#Cruz', 'Cruz', '#TedCruz', 'Ted Cruz',
+                                                                        '#Kasich', 'Kasich', '#JohnKasich', 'John Kasich',
+                                                                        '#Rubio', 'Rubio', '#Marco', 'Marco', '#MarcoRubio', 'Marco Rubio',
+                                                                        '#Sanders', 'Sanders', '#Bernie', 'Bernie', '#BernieSanders', 'Bernie Sanders',
+                                                                        '#Trump', 'Trump', '#Donald', 'Donald', '#DonaldTrump', 'Donald Trump'])
 
-
-def clinton_fetch():
-    clinton_stream = Stream(auth, TweetListener("clinton")).filter(track=['#Clinton'], async=True)
-
-
-def cruz_fetch():
-    cruz_stream = Stream(auth, TweetListener("cruz")).filter(track=['#Cruz'], async=True)
-
-
-def kasich_fetch():
-    kasich_stream = Stream(auth, TweetListener("kasich")).filter(track=['#Kasich'], async=True)
-
-
-def rubio_fetch():
-    rubio_stream = Stream(auth, TweetListener("rubio")).filter(track=['#Rubio'], async=True)
-
-
-def sanders_fetch():
-    sanders_stream = Stream(auth, TweetListener("sanders")).filter(track=['#Sanders'], async=True)
-
-
-def trump_fetch():
-    trump_stream = Stream(auth, TweetListener("trump")).filter(track=['#Trump'], async=True)
-
-
-carson_thread = Thread(target=carson_fetch)
-clinton_thread = Thread(target=clinton_fetch)
-cruz_thread = Thread(target=cruz_fetch)
-kasich_thread = Thread(target=kasich_fetch)
-rubio_thread = Thread(target=cruz_fetch)
-sanders_thread = Thread(target=sanders_fetch)
-trump_thread = Thread(target=trump_fetch)
-
-carson_thread.start()
-clinton_thread.start()
-cruz_thread.start()
-kasich_thread.start()
-rubio_thread.start()
-sanders_thread.start()
-trump_thread.start()
--- preprocessing.py
+++ preprocessing.py
@@ -6,7 +6,8 @@ author: Ross Tripi
 import json, re, os, operator, string
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from collections import Counter
+from nltk import bigrams
+from collections import Counter, defaultdict
 
 emoticons_str = r"""
     (?:
@@ -34,6 +35,8 @@ emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
 punctuation = list(string.punctuation)
 stop = stopwords.words('english') + punctuation + ['rt', 'via', '…', 'https']
 
+cooccurrence_matrix = defaultdict(lambda: defaultdict(int))
+
 
 def tokenize(s):
     return tokens_re.findall(s)
@@ -47,15 +50,40 @@ def preprocess(s, lowercase=False):
 
 
 for filename in os.listdir('corpora'):
+    print("Processing file: {}".format(filename))
     with open('corpora/' + filename, 'r') as jsonfile:
-        count_all = Counter()
-        print("Tweets for " + filename + ":")
+        count_singletons = Counter()
+        count_bigrams = Counter()
+        #print("Tweets for " + filename + ":")
         for line in jsonfile:
             if line not in ['\n', '\r\n']:
+                #print("Tokenizing the line: \n{}".format(line))
                 tweet = json.loads(line)
-                terms_all = [term for term in preprocess(tweet['text']) if term.lower() not in stop]
-                count_all.update(terms_all)
-                # tokens = preprocess(tweet['text'])
-                # print(tokens)
-        print(count_all.most_common(5))
+                if 'text' in tweet:
+                    # create list of single terms
+                    terms_all = [term.lower() for term in preprocess(tweet['text']) if term.lower() not in stop]
+                    # create list of bigrams
+                    bigrams_all = bigrams([term.lower() for term in preprocess(tweet['text'])])
+                    # update frequency distributions for singletons and bigrams
+                    count_singletons.update(terms_all)
+                    count_bigrams.update(bigrams_all)
+                    # update co-occurrence matrix
+                    for i in range(len(terms_all)-1):
+                        for j in range(i+1, len(terms_all)):
+                            word1, word2 = sorted([terms_all[i], terms_all[j]])
+                            if word1 != word2:
+                                cooccurrence_matrix[word1][word2] += 1
+
+                    # tokens = preprocess(tweet['text'])
+                    # print(tokens)
+        # print most common singletons, bigrams, and co-occurrences
+        print(count_singletons.most_common(25))
+        print(count_bigrams.most_common(25))
+        com_max = []
+        for term1 in cooccurrence_matrix:
+            term1_max_terms = max(cooccurrence_matrix[term1].items(), key=operator.itemgetter(1))[:5]
+            for term2 in term1_max_terms:
+                com_max.append(((term1, term2), cooccurrence_matrix[term1][term2]))
+        terms_max = sorted(com_max, key=operator.itemgetter(1), reverse=True)
+        print(terms_max[:5])
 
